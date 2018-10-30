0e83daaca754f8ebd7ae552bbae099aef9b9d0c0
lkearley <laurenkearley96@gmail.com>
2018-10-30 17:11:21 -0400

    First working commit

---
+++ Assets/Scripts/NavigationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationController : MonoBehaviour {
+
+    GameObject book;
+    bool isBookInfoActive = true;
+    bool isShelfViewActive = false;
+    GameObject shelf;
+
+    // Use this for initialization
+    void Start () {
+        book = GameObject.Find("Book Info Display");
+        shelf = GameObject.Find("Shelf");
+        book.SetActive(isBookInfoActive);
+        shelf.SetActive(isShelfViewActive);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            isBookInfoActive = !isBookInfoActive;
+            isShelfViewActive = !isShelfViewActive;
+            book.SetActive(isBookInfoActive);
+            shelf.SetActive(isShelfViewActive); 
+        }
+	}
+}
